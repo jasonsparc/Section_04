b98bf4403206c4bf15c50e8ed9c92d1a06993380
jason <jason.sparc.7@gmail.com>
2017-08-23 20:58:39 +0800

    Minor fixes

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -16,6 +16,7 @@ AProjectile::AProjectile()
 	CollisionMesh->SetVisibility(false);
 
 	LaunchBlast = CreateDefaultSubobject<UParticleSystemComponent>(FName("Launch Blast"));
+	LaunchBlast->AttachTo(RootComponent);
 
 	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement Component"));
 	ProjectileMovementComponent->bAutoActivate = false;
